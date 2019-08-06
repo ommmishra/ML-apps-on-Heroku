b5fc1273b9cf0f05385ddf9296f7b09184244d60
Omm <ommmishra830@gmail.com>
2019-08-07 02:10:02 +0530

    implemnting functions

--- app/server.py
+++ app/server.py
@@ -1,14 +1,18 @@
 from fastai import *
 from fastai.vision import *
 
-learner = None
+learn = None
 
 def imagePredictorModel(path_to_export, export_file_name):
-	learner = load_learner(path_to_export, export_file_name)
+	learn = load_learner(path_to_export, export_file_name)
 	
 
-def predict(request):
+def predictCar(request):
 	img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
+    prediction = learn.predict(img)
+    return prediction 
 
+def homepage(request):
+	return render_template("index.html")
