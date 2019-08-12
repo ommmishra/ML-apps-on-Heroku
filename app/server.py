9bf11ce768bffbb6020780438cb302ab0f9a1a51
Omm <ommmishra830@gmail.com>
2019-08-13 02:26:00 +0530

    Added Flask Support

--- app/server.py
+++ app/server.py
@@ -3,26 +3,30 @@ from fastai import *
 from fastai.vision import *
 from gevent.pywsgi import WSGIServer
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static")
 
-path = Path("/home/omm/WorkStuff/herokuDeploy/ML-apps-on-Heroku/app/models")
-imgPath = Path("/home/omm/Downloads/wall-1564639504.png")
-learn = load_learner(path_to_export)
-print('yes')
+# path = Path("/home/omm/WorkStuff/herokuDeploy/ML-apps-on-Heroku/app/models")
+# imgPath = Path("/home/omm/Downloads/wall-1564639504.png")
 
-
-def imagePredictorModel(path_to_export):
+try:
 	learn = load_learner(path_to_export)
 	print('yes')
-	return learn
+except RuntimeError as e:
+	print(e)
 
-def predictCar(learn, imgPath):
-	# img_data = await request.form()
- #    img_bytes = await (img_data['file'].read())
- #    img = open_image(BytesIO(img_bytes))
-    img = open_image(imgPath)
-    pred_class, pred_tensor, predictionSet = learn.predict(img)
-    return predictionSet 
+@app.route("/", methods=['GET'])
+def index():
+	return render_template('index.html')
+
+
+@app.route("/analyze", methods=['POST']) 	
+def predictCar(learn):
+	img_data = await request.form()
+    img_bytes = await (img_data['file'].read())
+    img = open_image(BytesIO(img_bytes))
+    predictionSet = learn.predict(img)
+    prediction = predictionSet(img)[0] 
+    return jsonify('result': str(prediction))
 
 # def homepage(request):
 	# return render_template("index.html")
@@ -31,3 +35,8 @@ def predictCar(learn, imgPath):
 # x = predictCar(learn, imgPath)
 # print(x[2])
 # print(type(x[2]))
+
+if __name__ == '__main__':
+	port = int(os.environ.get('PORT', 5000))
+	http_server = WSGIServer(('',port),app)
+	http_server.serve_forever()
